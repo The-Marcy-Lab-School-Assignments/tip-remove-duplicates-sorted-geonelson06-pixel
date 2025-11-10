0c04101e34d11324b44ff0c7e92f21b6a5d5e3c0
Gabriel <geonelson06@gmail.com>
2025-11-10 10:15:45 -0500

    Two pointer

--- index.js
+++ index.js
@@ -15,7 +15,19 @@
  */
 
 const removeDuplicates = (nums) => {
-    //write your code here
+  if (nums.length === 0) return 0;
+
+  let i = 0;
+
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+
+  return i + 1;
+
 };
 
 // Example usage (commented out to avoid interference with tests)
